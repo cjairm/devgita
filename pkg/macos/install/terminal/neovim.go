78e7c6cde2a3c6e6ef6655a4808f7665134db041
Carlos Mendez <carlos@hadaelectronics.com>
2024-11-14 20:07:27 -0600

    Add neovim configuration file
    inspired on kickstart - neovim: https://github.com/nvim-lua/kickstart.nvim

---
+++ pkg/macos/install/terminal/neovim.go
@@ -0,0 +1,12 @@
+// -------------------------
+// TODO: Write documentation how to use this
+// - Kickstart documentation: https://github.com/nvim-lua/kickstart.nvim?tab=readme-ov-file
+// - Personal configuration: https://github.com/cjairm/devenv/blob/main/nvim/init.lua
+// - Releases: https://github.com/neovim/neovim/releases
+// -------------------------
+
+package macos
+
+func InstallNeovim() error {
+	return nil
+}
