97cdea2d51b1d10f348a590dfca75fe42f4efba7
Jair Mendez <carlos@hadaelectronics.com>
2025-01-23 12:44:52 -0600

    After selecting the right theme

--- pkg/configs/neovim/init.lua
+++ pkg/configs/neovim/init.lua
@@ -910,23 +910,10 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  -- Custom ***
+  -- NOTE: Remove any other theme
+  { 'ellisonleao/gruvbox.nvim', priority = 1000 },
+  -- ***
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -1097,6 +1084,10 @@ vim.g.NERDTreeQuitOnOpen = 1
 
 -- Enable relative number
 vim.o.relativenumber = true
+
+-- Theme
+vim.o.background = 'dark'
+vim.cmd [[colorscheme gruvbox]]
 
 -- [[ NERDTree ]]
 vim.keymap.set('n', '<leader>nt', ':NERDTreeToggle<CR>', { desc = '[N]erdTree [T]oggle' })
