3423f42b32f0099c4d5a5432714c8131c34d572a
Jair Mendez <jair.mendez@lever.co>
2025-10-31 18:45:58 -0500

    Random fixes

--- internal/apps/mise/mise_test.go
+++ internal/apps/mise/mise_test.go
@@ -43,21 +43,3 @@ func TestSoftInstall(t *testing.T) {
 		t.Fatalf("expected MaybeInstallPackage(mise), got %q", mc.MaybeInstalled)
 	}
 }
-
-func TestExecuteCommand(t *testing.T) {
-	// These tests use the actual BaseCommand but won't execute actual mise commands
-	// since we expect mise to fail in test environment
-	mc := commands.NewMockCommand()
-	app := &Mise{Cmd: mc}
-
-	// Test that methods don't panic - they will fail due to mise not being available
-	// but that's expected in test environment
-	err := app.ExecuteCommand("--version")
-
-	// Should fail since mise isn't available, but shouldn't panic
-	if err == nil {
-		t.Log("Mise commands succeeded unexpectedly (mise must be available)")
-	} else {
-		t.Log("Expected failure due to mise not being available in test environment")
-	}
-}
