d511e4aba42fdbeb8334160467425f419905386c
Carlos Mendez <carlos@hadaelectronics.com>
2026-05-16 11:31:53 -0500

    feat: register missing which-key groups and leader f keymap

--- configs/neovim/lua/kickstart/plugins/which_key.lua
+++ configs/neovim/lua/kickstart/plugins/which_key.lua
@@ -11,5 +11,13 @@ require("which-key").setup({
 		{ "<leader>t", group = "[T]oggle" },
 		{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } }, -- Enable gitsigns recommended keymaps first
 		{ "gr", group = "LSP Actions", mode = { "n" } },
+		-- [devgita]
+		{ "<leader>c", group = "[C]opy path" },
+		{ "<leader>b", group = "[B]uffer" },
+		{ "<leader>p", group = "[P]lugins" },
+		{ "<leader>w", group = "[W]indow" },
+		{ "<leader>l", group = "[L]azygit" },
+		{ "<leader>f", desc = "[F]ormat buffer", mode = { "n", "v" } },
+		-- [/devgita]
 	},
 })
